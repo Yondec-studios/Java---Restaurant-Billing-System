20d2c6b681c349fd59ba2e8cdd526c307a9768f1
Tharanga Sandun <yondec.studios@gmail.com>
2023-04-30 08:40:10 +0530

    Created a remote database

--- src/dataBaseQuaries.java
+++ src/dataBaseQuaries.java
@@ -1,8 +1,8 @@
 public interface dataBaseQuaries{
-    //String url = "jdbc:mysql://sql12.freesqldatabase.com:3306/sql12601094";
-    String url = "jdbc:mysql://localhost:3306/sql12601094";
+    String url = "jdbc:mysql://mysql-123983-0.cloudclusters.net:18755/sql12601094";
+    //String url = "jdbc:mysql://localhost:3306/sql12601094";
     //String url = "jdbc:mysql://db4free.net:3306/sql12601094";
-    //String user = "sql12601094";
-    String user = "root";
+    String user = "admin";
+    //String user = "root";
     String dbPassword = "3vhlrEGNSd";
 }
